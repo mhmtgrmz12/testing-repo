c61e6968c471602b6dbde5c81945c6de69c46f0a
mhmtgrmz12 <148793358+mhmtgrmz12@users.noreply.github.com>
2025-05-28 02:35:51 +0200

    fixed

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from random import randint
 
 app = FastAPI()
-
-@app.get("/")
+@app.get("/")  
 async def get_random_percentage():
     return {'percentage': randint(0, 100)}
